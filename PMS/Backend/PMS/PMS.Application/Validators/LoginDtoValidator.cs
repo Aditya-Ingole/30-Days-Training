31425ab61940f9f21de28486775806a2ba24d133
Aditya-Ingole <a13ingole@gmail.com>
2026-06-11 22:57:17 +0530

    Recovered clean PMS Backend

---
+++ PMS/Backend/PMS/PMS.Application/Validators/LoginDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using PMS.Application.DTOs.Auth;
+
+namespace PMS.Application.Validators;
+
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage(
+                "Email is required.")
+            .EmailAddress()
+            .WithMessage(
+                "Invalid email format.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage(
+                "Password is required.");
+    }
+}
